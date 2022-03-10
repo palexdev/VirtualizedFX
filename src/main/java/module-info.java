2098ab4c8cdf43d33fe056c2e185555ca3a4250e
Alessadro Parisi <alessandro.parisi406@gmail.com>
2022-03-10 21:19:25 +0100

    Few changes to module-info.java
    Minor style changes, also make JavaFX dependencies transitive

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,17 +1,23 @@
-module virtualizedfx {
-	requires javafx.base;
-	requires javafx.graphics;
-	requires javafx.controls;
+module VirtualizedFX {
+	requires transitive javafx.base;
+	requires transitive javafx.graphics;
+	requires transitive javafx.controls;
 
 	exports io.github.palexdev.virtualizedfx;
-
+	
+	// Beans Package
 	exports io.github.palexdev.virtualizedfx.beans;
-	exports io.github.palexdev.virtualizedfx.cache;
+	
+	// Cell Package
 	exports io.github.palexdev.virtualizedfx.cell;
+	
+	// Collections Package
 	exports io.github.palexdev.virtualizedfx.collections;
-
+	
+	// Flow Package
 	exports io.github.palexdev.virtualizedfx.flow.base;
 	exports io.github.palexdev.virtualizedfx.flow.simple;
-
+	
+	// Utils Package
 	exports io.github.palexdev.virtualizedfx.utils;
 }
